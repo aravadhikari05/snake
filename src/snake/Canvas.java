36c828a7d265e4a2962e0381d7e9ea9be959a79b
aravadhikari05 <aravadhikari05@github.com>
2021-03-18 00:38:16 -0700

    changed color scheme, created border, fix double key press bug

--- src/snake/Canvas.java
+++ src/snake/Canvas.java
@@ -9,7 +9,7 @@ public class Canvas extends JPanel  {
 
     public static final int TILE_SIZE = 20; //size of each tile
     public static final int GRID_SIZE = 19; // number of tiles on a board
-    public static final int WINDOW_SIZE = TILE_SIZE * GRID_SIZE; //size of window
+    public static final int WINDOW_SIZE = TILE_SIZE * GRID_SIZE + (2 * TILE_SIZE); //size of window
 
     private Game game;
     private Timer timer;
@@ -22,7 +22,7 @@ public class Canvas extends JPanel  {
 
     private void initPanel() {
         addKeyListener(game. new Input());
-        setBackground(Color.BLACK);
+        setBackground(new Color(30, 0, 0));
         setPreferredSize(new Dimension(WINDOW_SIZE,WINDOW_SIZE));
         setFocusable(true);
     }
@@ -42,26 +42,33 @@ public class Canvas extends JPanel  {
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        /*
-        for(int i = 0; i < GRID_SIZE; i++) {
-            for(int j = 0; j < GRID_SIZE; j++) {
-                g.setColor(Color.DARK_GRAY);
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        for(int i = 1; i <= GRID_SIZE; i++) {
+            for(int j = 1; j <= GRID_SIZE; j++) {
+                g2d.setColor(new Color(60, 10, 10));
                 if((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) {
-                    g.fillRect(j * TILE_SIZE,i * TILE_SIZE,TILE_SIZE, TILE_SIZE);
+                    g2d.fillRect(j * TILE_SIZE,i * TILE_SIZE,TILE_SIZE, TILE_SIZE);
                 }
             }
-        } */
-
-        g.setColor(Color.WHITE);
-        g.setFont(new Font("Courier", Font.PLAIN, 20));
-        g.drawString("" + game.getScore(), 5 , 20);
-
-        g.setColor(Color.RED);
-        g.fillRect(game.getAplX() * TILE_SIZE + 1 , game.getAplY() * TILE_SIZE + 1, TILE_SIZE-2, TILE_SIZE-2);
-
-        g.setColor(Color.GREEN);
-        for(int i = 0; i < game.getTailList().size(); i++) {
-            g.fillRect(game.getTailList().get(i).x * TILE_SIZE + 1,game.getTailList().get(i).y * TILE_SIZE + 1, TILE_SIZE-2, TILE_SIZE-2);
         }
+
+        g2d.setColor(new Color(10, 75, 200));
+        g2d.fillRect(game.getAplX() * TILE_SIZE + 1 , game.getAplY() * TILE_SIZE + 1, TILE_SIZE-2, TILE_SIZE-2);
+
+        g2d.setColor(new Color(20, 160, 15));
+        for(int i = 0; i < game.getTailList().size(); i++) {
+            g2d.fillRect(game.getTailList().get(i).x * TILE_SIZE + 1,game.getTailList().get(i).y * TILE_SIZE + 1, TILE_SIZE-2, TILE_SIZE-2);
+        }
+
+        g2d.setColor(new Color(15, 5, 5));
+        g2d.setStroke(new BasicStroke(TILE_SIZE));
+        g2d.drawRect(10,10, WINDOW_SIZE-20, WINDOW_SIZE-20);
+
+        g2d.setColor(Color.WHITE);
+        g2d.setFont(new Font("Courier", Font.PLAIN, 20));
+        g2d.drawString("" + game.getScore(), 5 , 20);
+
     }
 }
--- src/snake/Game.java
+++ src/snake/Game.java
@@ -12,13 +12,15 @@ import java.util.Random;
 public class Game {
 
     private static final int START_LENGTH = 2;
-    private static final int START_POSITION = Canvas.GRID_SIZE/2;
+    private static final int START_POSITION = (int) Math.ceil(1.0 * Canvas.GRID_SIZE/2);
     private static final int START_APPLE = 5;
 
     private int posX, posY,
                 dirX, dirY,
                 aplX, aplY,
                 tail, score;
+
+    private boolean keyPressed;
 
     private ArrayList<Point> tailList;
 
@@ -39,13 +41,14 @@ public class Game {
         posX += dirX;
         posY += dirY;
 
+        keyPressed = false;
+
         checkApl();
         manageLength();
         if(wallCollide() || selfCollide()) {
             try {
                 if(dirX != 0 || dirY != 0) {
                     playSound(death);
-
                 }
             } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
                 e.printStackTrace();
@@ -62,8 +65,8 @@ public class Game {
             } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
                 e.printStackTrace();
             }
-            aplX = rand.nextInt(Canvas.GRID_SIZE);
-            aplY = rand.nextInt(Canvas.GRID_SIZE);
+            aplX = rand.nextInt(Canvas.GRID_SIZE) + 1;
+            aplY = rand.nextInt(Canvas.GRID_SIZE) + 1;
 
             tail++;
             score++;
@@ -78,7 +81,7 @@ public class Game {
     }
 
     private boolean wallCollide() {
-        return posY > Canvas.GRID_SIZE - 1 || posY < 0 || posX > Canvas.GRID_SIZE - 1 || posX < 0;
+        return posY > Canvas.GRID_SIZE || posY < 1 || posX > Canvas.GRID_SIZE || posX < 1;
     }
 
     private boolean selfCollide() {
@@ -91,6 +94,7 @@ public class Game {
     }
 
     private void reset() {
+        keyPressed = false;
         tail = START_LENGTH;
         posX = posY = START_POSITION;
         dirX = dirY = 0;
@@ -108,6 +112,9 @@ public class Game {
     public class Input extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e) {
+            if(keyPressed) {
+                return;
+            }
             int key = e.getKeyCode();
             if(key == KeyEvent.VK_W && dirY != 1) {
                 dirY = -1;
@@ -121,7 +128,10 @@ public class Game {
             } else if(key == KeyEvent.VK_D && dirX != -1) {
                 dirY = 0;
                 dirX = 1;
+            } else {
+                return;
             }
+            keyPressed = true;
         }
 
     }
